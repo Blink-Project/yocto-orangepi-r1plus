c8f2c0578add119d6141e0d77e7a3af3cea52635
Alexi Demers <alexidemers@gmail.com>
2023-01-08 15:59:39 -0500

    conf and setup.sh: fix git user issues and add ccache
    Git user config was not passed to the container, making devtool fail.
    
    ccache reduces build time fo individual packages

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ docker run \
     -it \
     --network host \
     -v "${PWD}:${PWD}" \
+    -v ~/.gitconfig:/home/pokyuser/.gitconfig:ro \
     -w "${PWD}" \
     crops/poky:ubuntu-20.04 \
     bash -c 'source layers/poky/oe-init-build-env && bash'
